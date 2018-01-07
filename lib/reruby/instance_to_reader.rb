f8859525e5d9e3b2c325cc24429ea7e73d335967
Diego Guerra <diego.guerra.suarez@gmail.com>
2018-01-07 12:37:35 +0100

    instance to reader command

--- lib/reruby/instance_to_reader.rb
+++ lib/reruby/instance_to_reader.rb
@@ -1,5 +1,30 @@
 module Reruby
   class InstanceToReader
 
+    def initialize(namespace: "", config: Config.default)
+      @namespace = namespace
+      @config = config
+      @ns_paths = NamespacePaths.new(namespace: namespace, paths: find_paths_using_class)
+    end
+
+    def perform
+      rewriter = Rewriter.new(namespace: namespace)
+      action = Actions::FileRewrite.new(path: ns_paths.main_file, rewriter: rewriter)
+      action.perform
+    end
+
+    private
+
+    attr_reader :namespace, :config, :ns_paths
+
+    def original_class_name
+      namespace.split("::").last
+    end
+
+    def find_paths_using_class
+      finder = FileFinder.new(config: config)
+      finder.paths_containing_word(original_class_name)
+    end
+
   end
 end
